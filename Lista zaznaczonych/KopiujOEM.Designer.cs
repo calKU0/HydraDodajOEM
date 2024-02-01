742029005fdd6e89af574ff781ff079dce76a460
Krzysztof Kurowski <228528@student.uek.krakow.pl>
2024-02-01 12:19:32 +0100

    add new form

---
+++ Lista zaznaczonych/KopiujOEM.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace DodajOem
+{
+    partial class KopiujOEM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.check = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.oem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dostawca = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pokazB2B = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.szukajB2B = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.kntGidNumer = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(153, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(410, 29);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(12, 22);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(135, 19);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Wpisz kod towaru";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.check,
+            this.oem,
+            this.dostawca,
+            this.pokazB2B,
+            this.szukajB2B,
+            this.kntGidNumer});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 67);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(551, 315);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.RowPrePaint += new System.Windows.Forms.DataGridViewRowPrePaintEventHandler(this.dgv_RowPrePaint);
+            // 
+            // check
+            // 
+            this.check.HeaderText = "";
+            this.check.Name = "check";
+            this.check.Width = 40;
+            // 
+            // oem
+            // 
+            this.oem.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.oem.HeaderText = "Kod OEM";
+            this.oem.Name = "oem";
+            this.oem.ReadOnly = true;
+            // 
+            // dostawca
+            // 
+            this.dostawca.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dostawca.HeaderText = "Dostawca";
+            this.dostawca.Name = "dostawca";
+            this.dostawca.ReadOnly = true;
+            // 
+            // pokazB2B
+            // 
+            this.pokazB2B.HeaderText = "Pokaz w B2B";
+            this.pokazB2B.Name = "pokazB2B";
+            this.pokazB2B.ReadOnly = true;
+            this.pokazB2B.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.pokazB2B.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.pokazB2B.Width = 70;
+            // 
+            // szukajB2B
+            // 
+            this.szukajB2B.HeaderText = "Szukaj w B2B";
+            this.szukajB2B.Name = "szukajB2B";
+            this.szukajB2B.ReadOnly = true;
+            this.szukajB2B.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.szukajB2B.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.szukajB2B.Width = 70;
+            // 
+            // kntGidNumer
+            // 
+            this.kntGidNumer.HeaderText = "kntGidNumer";
+            this.kntGidNumer.Name = "kntGidNumer";
+            this.kntGidNumer.ReadOnly = true;
+            this.kntGidNumer.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 405);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Kopiuj wszystkie";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(429, 405);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Kopiuj zaznaczone";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // KopiujOEM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "KopiujOEM";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Kopiuj Oemy";
+            this.Load += new System.EventHandler(this.KopiujOEM_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn check;
+        private System.Windows.Forms.DataGridViewTextBoxColumn oem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dostawca;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn pokazB2B;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn szukajB2B;
+        private System.Windows.Forms.DataGridViewTextBoxColumn kntGidNumer;
+    }
+}
---
+++ Lista zaznaczonych/KopiujOEM.cs
@@ -0,0 +1,194 @@
+﻿using Hydra;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace DodajOem
+{
+    public partial class KopiujOEM : Form
+    {
+        CheckBox headerCheck = new CheckBox();
+
+        private string connectionString;
+        private string[] kodyOEM;
+        private string towarKod;
+        private int towarGid;
+        public List<Row> ReturnList = new List<Row>();
+
+        public KopiujOEM(string connectionString)
+        {
+            this.connectionString = connectionString;
+            InitializeComponent();
+        }
+        private void KopiujOEM_Load(object sender, EventArgs e)
+        {
+            Rectangle szukajB2BHeader = this.dataGridView1.GetCellDisplayRectangle(this.dataGridView1.Columns.IndexOf(this.check), -1, true);
+
+            headerCheck.Location = new Point(szukajB2BHeader.Location.X, szukajB2BHeader.Location.Y + (szukajB2BHeader.Height - szukajB2BHeader.Height) / 2);
+            headerCheck.Click += new EventHandler(headerCheck_Click);
+            headerCheck.FlatStyle = FlatStyle.Flat;
+            headerCheck.BackColor = Color.Transparent;
+            headerCheck.ForeColor = Color.Transparent;
+            headerCheck.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            headerCheck.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            headerCheck.FlatAppearance.BorderSize = 0;
+            dataGridView1.Controls.Add(headerCheck);
+        }
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                dataGridView1.Rows.Clear();
+                towarKod = textBox1.Text;
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string queryGid = "SELECT Twr_GidNumer from cdn.TwrKarty where twr_kod = '" + towarKod + "'";
+                    string query = "SELECT TKO_OEM, Knt_Nazwa1, TKO_KntNumer, TKO_SzukajB2B, TKO_PokazB2B FROM dbo.TwrKodyOEM LEFT JOIN cdn.KntKarty on knt_gidnumer = TKO_KntNumer WHERE TKO_TwrNumer = @GidNumer";
+                    connection.Open();
+                    SqlCommand commandGid = new SqlCommand(queryGid, connection);
+                    towarGid = Convert.ToInt32(commandGid.ExecuteScalar());
+
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@GidNumer", towarGid);
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            int index = dataGridView1.Rows.Add(false,reader["TKO_OEM"], reader["Knt_Nazwa1"] == DBNull.Value ? null : reader["Knt_Nazwa1"], Convert.ToBoolean(reader["TKO_PokazB2B"]), Convert.ToBoolean(reader["TKO_SzukajB2B"]), reader["TKO_KntNumer"] == DBNull.Value ? null : reader["TKO_KntNumer"]);
+                        }
+                    }
+                    else
+                    {
+                        reader.Close();
+                        query = "SELECT TPO_OpisKrotki FROM cdn.twrAplikacjeOpisy with (nolock) WHERE TPO_JezykId = 0 AND TPO_ObiNumer = @GidNumer";
+                        command = new SqlCommand(query, connection);
+                        command.Parameters.AddWithValue("@GidNumer", towarGid);
+                        reader = command.ExecuteReader();
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                kodyOEM = reader["TPO_OpisKrotki"].ToString().Split(',');
+                            }
+                            foreach (string kod in kodyOEM)
+                            {
+                                dataGridView1.Rows.Add(false,kod.Trim(), null, false, false, null);
+                            }
+                        }
+                    }
+                }
+                if (dataGridView1.Rows.Count < 1)
+                {
+                    MessageBox.Show("Brak towaru z podanym kodem", "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    try
+                    {
+                        Row row1 = new Row();
+                        row1.Oem = row.Cells["oem"].Value.ToString();
+                        row1.Dostawca = row.Cells["dostawca"].Value.ToString();
+                        row1.PokazB2B = Convert.ToBoolean(row.Cells["pokazB2B"].Value);
+                        row1.SzukajB2B = Convert.ToBoolean(row.Cells["szukajB2B"].Value);
+                        row1.KntGidNumer = row.Cells["kntGidNumer"].Value.ToString();
+                        ReturnList.Add(row1);
+                    }
+                    catch (NullReferenceException)
+                    {
+                        Row row1 = new Row();
+                        row1.Oem = row.Cells["oem"].Value.ToString();
+                        row1.PokazB2B = Convert.ToBoolean(row.Cells["pokazB2B"].Value);
+                        row1.SzukajB2B = Convert.ToBoolean(row.Cells["szukajB2B"].Value);
+                        ReturnList.Add(row1);
+                    }
+                    finally
+                    {
+                        DialogResult = DialogResult.OK;
+                        this.Close();
+                    }
+                }
+
+                DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            catch (Exception ex) { MessageBox.Show("Błąd w kopiowaniu " + ex.ToString(), "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+        }
+
+        private void headerCheck_Click(object sender, EventArgs e)
+        {
+            dataGridView1.EndEdit();
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                row.Cells["check"].Value = headerCheck.Checked;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    if ((bool)row.Cells["check"].Value == true)
+                    {
+                        try
+                        {
+                            Row row1 = new Row();
+                            row1.Oem = row.Cells["oem"].Value.ToString();
+                            row1.Dostawca = row.Cells["dostawca"].Value.ToString();
+                            row1.PokazB2B = Convert.ToBoolean(row.Cells["pokazB2B"].Value);
+                            row1.SzukajB2B = Convert.ToBoolean(row.Cells["szukajB2B"].Value);
+                            row1.KntGidNumer = row.Cells["kntGidNumer"].Value.ToString();
+                            ReturnList.Add(row1);
+                        }
+                        catch (NullReferenceException)
+                        {
+                            Row row1 = new Row();
+                            row1.Oem = row.Cells["oem"].Value.ToString();
+                            row1.PokazB2B = Convert.ToBoolean(row.Cells["pokazB2B"].Value);
+                            row1.SzukajB2B = Convert.ToBoolean(row.Cells["szukajB2B"].Value);
+                            ReturnList.Add(row1);
+                        }
+                        finally
+                        {
+                            DialogResult = DialogResult.OK;
+                            this.Close();
+                        }
+                    }
+                }
+                DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            catch (Exception ex) { MessageBox.Show("Błąd w kopiowaniu " + ex.ToString(), "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+        }
+        private void dgv_RowPrePaint(object sender, DataGridViewRowPrePaintEventArgs e)
+        {
+            e.PaintParts &= ~DataGridViewPaintParts.Focus;
+        }
+    }
+    public class Row
+    {
+        public string Oem;
+        public string Dostawca = null;
+        public bool PokazB2B;
+        public bool SzukajB2B;
+        public string KntGidNumer = null;
+    }
+}
